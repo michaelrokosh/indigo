259c5eb3589ccbd5459a9800b84723e7bde8845b
Michael Rokosh <mike.rokosh@gmail.com>
2014-01-17 14:40:37 -0500

    vk3

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def vkontakte
-  	<hh user=user> = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
-    if <hh user=user>.persisted?
+  	@user = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
+    if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Vkontakte"
-      sign_in_and_redirect <hh user=user>, :event => :authentication
+      sign_in_and_redirect @user, :event => :authentication
     else
       flash[:notice] = "authentication error"
       redirect_to root_path

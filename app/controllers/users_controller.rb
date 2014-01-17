67b2073b7fa1e3a35d035672dc77c03790c4dc72
Michael Rokosh <mike.rokosh@gmail.com>
2014-01-17 15:13:06 -0500

    vk7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,4 +39,14 @@
 	      end
 	    end
   	end
+
+  	def destroy
+	    @user = User.find(params[:id])
+	    @user.destroy
+
+	    respond_to do |format|
+	      format.html { redirect_to users_url }
+	      format.json { head :no_content }
+	    end
+  	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ActiveRecord::Base
   acts_as_voter
 
   def self.find_for_vkontakte_oauth access_token
-    if user = User.where(:url => access_token.info.urls.Vkontakte).first
+    if user = User.where(url: access_token.info.urls.Vkontakte).first
       user
     else 
-      User.create!(:provider => access_token.provider, :url => access_token.info.urls.Vkontakte, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => access_token.extra.raw_info.domain+'@vk.com', :password => Devise.friendly_token[0,20]) 
+      User.create!(provider: access_token.provider, url: access_token.info.urls.Vkontakte, username: access_token.info.name, nickname: access_token.extra.raw_info.domain, email: access_token.extra.raw_info.domain + "@vk.com", password: Devise.friendly_token[0,20]) 
     end
   end
 
